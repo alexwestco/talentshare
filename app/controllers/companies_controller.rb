d96e6fe20801d76261443293dc1026c2817461c1
alexandersideris <alexander.sideris.west@gmail.com>
2018-02-21 18:32:15 +0200

    Added bootstrap support

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -4,15 +4,49 @@ class CompaniesController < ApplicationController
 	end
 
 	def new
+		@company = Company.new
 
 	end
 
 	def create
+	    @company = Company.new(post_params)
 
+	    if @company.save
+	      redirect_to "/"
+	    else
+	      render 'new'
+	    end
+  	end
+
+	def destroy
+		@company = Company.find(params[:id])
+
+	    @company.destroy
+	    redirect_to "/"
 	end
 
-	def show
-
+	def edit
+		@company = Company.find(params[:id]) 
 	end
-	
+
+  	def update
+    	@company = Company.find(params[:id])
+
+    	if @company.update(post_params)
+      		redirect_to "/"
+    	else
+      		render 'edit'
+    	end
+  	end
+
+  	def show
+    	@company = Company.find(params[:id])
+  	end
+
+  	private
+
+	def post_params
+		params.require(:company).permit(:name, :description, :url, :location)
+	end
+
 end
