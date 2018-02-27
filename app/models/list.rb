f071269b27b88c2858ff3b48cede5f1e1136fef8
alexandersideris <alexander.sideris.west@gmail.com>
2018-02-27 13:48:17 +0200

    Added excel support

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,9 @@
 class List < ApplicationRecord
 	has_attached_file :file
-	validates_attachment :file, content_type: { content_type: ["application/pdf", "image/jpeg", "image/jpg", "application/msword", "application/vnd.openxmlformats-officedocument.wordprocessingml.document"] }
+	validates_attachment :file, content_type: { content_type: [
+		"application/pdf",
+		"application/msword",
+		"application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+		"application/vnd.ms-excel",
+		"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"] }
 end
